da126183cb20078135fc5abe4fefc448663cc3ed
Naufal37 <nisizza@gmail.com>
2026-08-25 19:24:36 +0700

    Update db.js

--- db.js
+++ db.js
@@ -5,21 +5,17 @@ let playlistSongs = [];
 
 module.exports = {
   all: (sql, params, callback) => {
-    if (typeof params === 'function') {
-      callback = params;
-    }
-    if (sql.includes('playlists')) return callback(null, playlists);
+    if (typeof params === 'function') callback = params;
+    if (sql && sql.includes('playlists')) return callback(null, playlists);
     callback(null, songs);
   },
   run: (sql, params, callback) => {
-    if (typeof params === 'function') {
-      callback = params;
-    }
-    if (sql.includes('INSERT INTO songs')) {
+    if (typeof params === 'function') callback = params;
+    if (sql && sql.includes('INSERT INTO songs')) {
       const newSong = { id: Date.now(), title: params[0], artist: params[1], src: params[2], cover: params[3] };
       songs.push(newSong);
       if (callback) callback.call({ lastID: newSong.id }, null);
-    } else if (sql.includes('INSERT INTO playlists')) {
+    } else if (sql && sql.includes('INSERT INTO playlists')) {
       const newPlaylist = { id: Date.now(), name: params[0] };
       playlists.push(newPlaylist);
       if (callback) callback.call({ lastID: newPlaylist.id }, null);
